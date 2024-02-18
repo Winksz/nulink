1ab6bd0578e9d125febf61b05068235f69471323
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-02-18 10:16:19 +0200

    Update test7.sh

--- test7.sh
+++ test7.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
 print_menu() {
-    dialog --clear --backtitle "Оберіть опцію:" \
-           --menu "Виберіть одну з наступних опцій:" 15 50 4 \
-           1 "Опція 1" \
-           2 "Опція 2" \
-           3 "Опція 3" \
-           4 "Вихід" \
-           2> /tmp/menu_choice.txt
-    choice=$(cat /tmp/menu_choice.txt)
-    rm /tmp/menu_choice.txt
-    echo "$choice"
+    echo "Оберіть опцію:"
+    echo "1. Опція 1"
+    echo "2. Опція 2"
+    echo "3. Опція 3"
+    echo "4. Вихід"
 }
 
+current_choice=1
+print_menu
+
 while true; do
-    current_choice=$(print_menu)
-    case "$current_choice" in
-        1)
-            echo "Ви обрали Опцію 1"
+    read -rsn1 input
+    case "$input" in
+        $'\x1b') # Стрілка
+            read -rsn2 -t 0.001 key
+            case "$key" in
+                '[A') # Стрілка вгору
+                    ((current_choice--))
+                    ;;
+                '[B') # Стрілка вниз
+                    ((current_choice++))
+                    ;;
+            esac
             ;;
-        2)
-            echo "Ви обрали Опцію 2"
-            ;;
-        3)
-            echo "Ви обрали Опцію 3"
-            ;;
-        4)
-            echo "Ви обрали Вихід. До побачення!"
-            exit 0
+        $'\x0a') # Enter
+            case "$current_choice" in
+                1)
+                    echo "Ви обрали Опцію 1"
+                    ;;
+                2)
+                    echo "Ви обрали Опцію 2"
+                    ;;
+                3)
+                    echo "Ви обрали Опцію 3"
+                    ;;
+                4)
+                    echo "Ви обрали Вихід. До побачення!"
+                    exit 0
+                    ;;
+            esac
             ;;
     esac
+
+    if [ "$current_choice" -lt 1 ]; then
+        current_choice=1
+    elif [ "$current_choice" -gt 4 ]; then
+        current_choice=4
+    fi
+
+    print_menu
 done
